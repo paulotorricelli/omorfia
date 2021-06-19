45323eab06b081753d9a0fb7e7b6cec88e3d2b57
Paulo Torricelli <paulo.lima@r2sis.com.br>
2021-06-19 11:54:31 -0300

    Atualização do Link
    Omorfia

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://dev.com.br/omorfia/';
+	public $baseURL = 'https://omorfia.com.br/omorfia/';
 
 	/**
 	 * --------------------------------------------------------------------------
